d6c57b870a7b2041d5d6efbb760546a3a587363a
Aditya Gupta <adityaofficialgupta@gmail.com>
2019-12-18 18:19:59 +0000

    feat: save score, show score, show detailed game info

--- Guess the Flag/ContentView.swift
+++ Guess the Flag/ContentView.swift
@@ -8,10 +8,16 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @State private var showingScore = false
-    @State private var scoreTitle = ""
+struct GameState{
+    var currentScore = 0
+    var wasLastCorrect = false
+    var showingScore = false
+    var scoreTitle = ""
+}
 
+struct ContentView: View {
+    @State private var currentGame = GameState()
+    
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var selectedCountry = Int.random(in: 0...2)
 
@@ -20,28 +26,34 @@ struct ContentView: View {
             LinearGradient(gradient: Gradient(colors: [Color(UIColor.systemBackground), .accentColor]), startPoint: .top, endPoint: .bottom)
                 .edgesIgnoringSafeArea(.all)
 
-            VStack(spacing: 30) {
-                VStack {
-                    Text("Tap the flag of: ")
-                    Text("\(countries[selectedCountry])")
-                        .font(.largeTitle)
-                        .fontWeight(.black)
-                }
-                ForEach(0..<3) { number in
-                    Button(action: {
-                        self.flagTapped(number)
-                    }) {
-                        Image(self.countries[number])
-                            .renderingMode(.original)
-                            .clipShape(Capsule())
-                            .shadow(radius: 10)
-                            .overlay(Capsule().stroke(Color.white, lineWidth: 7))
+            VStack(spacing: 40) {
+                VStack(spacing: 30) {
+                    VStack {
+                        Text("Tap the flag of: ")
+                        Text("\(countries[selectedCountry])")
+                            .font(.largeTitle)
+                            .fontWeight(.black)
+                    }
+                    ForEach(0..<3) { number in
+                        Button(action: {
+                            self.flagTapped(number)
+                        }) {
+                            Image(self.countries[number])
+                                .renderingMode(.original)
+                                .clipShape(Capsule())
+                                .shadow(radius: 10)
+                                .overlay(Capsule().stroke(Color.white, lineWidth: 7))
+                        }
                     }
                 }
+
+                Text("Correct Guesses: \(currentGame.currentScore)")
+                    .font(.headline)
+                    .fontWeight(.black)
             }
         }
-            .alert(isPresented: $showingScore) {
-                Alert(title: Text("Your Choice was:"), message: Text(scoreTitle), dismissButton: .default(
+        .alert(isPresented: $currentGame.showingScore) {
+            Alert(title: currentGame.wasLastCorrect ? Text("Nice") : Text("Oops"), message: Text(currentGame.scoreTitle), dismissButton: .default(
                         Text("Continue"), action: {
                             self.askQuestion()
                         }
@@ -52,11 +64,14 @@ struct ContentView: View {
 
     func flagTapped(_ number: Int) {
         if number == selectedCountry {
-            scoreTitle = "Correct"
+            currentGame.scoreTitle = "That was the flag for  \(countries[number])"
+            currentGame.currentScore += 1
+            currentGame.wasLastCorrect = true
         } else {
-            scoreTitle = "Wrong"
+            currentGame.scoreTitle = "Wrong! That's the flag for \(countries[number])"
+            currentGame.wasLastCorrect = false
         }
-        self.showingScore = true
+        currentGame.showingScore = true
     }
 
     func askQuestion() {
